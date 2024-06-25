4b408799947a59654d28426ba346f284d1cc940c
kundalrahul <kundalrahul11@gmail.com>
2024-06-25 16:20:17 -0400

    adding repo

---
+++ appointment-tracker/src/main/java/com/example/appointment_tracker/AppointmentTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.appointment_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentTrackerApplication.class, args);
+	}
+
+}
